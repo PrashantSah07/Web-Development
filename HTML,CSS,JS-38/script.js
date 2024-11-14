67e8abe582adbc18a45545a10d65037277f559ac
prashant <prashantkumar855114@gmail.com>
2024-11-14 18:05:37 +0530

    Create Weather API

--- HTML,CSS,JS-38/script.js
+++ HTML,CSS,JS-38/script.js
@@ -1,3 +1,34 @@
+// let search = document.getElementById("serch-svg");
+// let input = document.getElementById("cityName");
+// let printData = document.querySelector(".data");
+
+
+// async function weatherReport() {
+
+//     let inputValue = input.value;
+//     console.log("Fetching data, please wait...");
+
+//     let response = fetch(`https://goweather.herokuapp.com/weather/${inputValue}`);
+
+//     try {
+//         let a = await response;
+//         let b = await (a.text());
+//         printData.innerHTML = b;
+//     }
+//     catch (error) {
+//         console.log(error)
+//     }
+//     finally {
+//         console.log("Done!")
+//     }
+
+// };
+
+// search.addEventListener("click", weatherReport);
+
+
+
+
 let search = document.getElementById("serch-svg");
 let input = document.getElementById("cityName");
 let printData = document.querySelector(".data");
@@ -12,12 +43,20 @@ async function weatherReport() {
 
     try {
         let a = await response;
-        let b = await (a.text());
-        printData.innerHTML = b;
+        let b = await a.json();
+        printData.innerHTML = ` 
+        <p>Location: ${inputValue}</p>
+        <p>Description: ${b.description}</p>
+        <p>Temperature: ${b.temperature}</p>
+        <p>Wind: ${b.wind}</p> 
+        <p>Forcast: ${b.forecast}</p>`
+
     }
+
     catch (error) {
         console.log(error)
     }
+
     finally {
         console.log("Done!")
     }
@@ -25,4 +64,3 @@ async function weatherReport() {
 };
 
 search.addEventListener("click", weatherReport);
-
