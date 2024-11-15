1f54336b9d150768e1f6520be6673ee69eec8c2e
prashant <prashantkumar855114@gmail.com>
2024-11-15 12:37:10 +0530

    Sending Post Request With Fetch API

--- HTML,CSS,JS-40/script.js
+++ HTML,CSS,JS-40/script.js
@@ -1,24 +1,53 @@
-// console.log(document.cookie);
-// document.cookie="name=prashant"
-// document.cookie="name2=prashant123"
-// console.log(document.cookie); 
+let data1 = (document.getElementsByClassName('data')[0]);
+let data2 = (document.getElementsByClassName('data')[1]);
+let data3 = (document.getElementsByClassName('data')[2]);
+
+let cut = document.querySelector('.cut');
+let searchButton = document.querySelector('#search-button');
+let inputSearch = document.querySelector('#input-search');
+let cityName = document.querySelector('.cityName');
 
 
+async function getData() {
+    cityName.innerHTML = inputSearch.value
+    cut.style.display = "block"
+    let inputValue = inputSearch.value;
+    console.log("fetching data, please wait...");
+    let p = fetch(`https://goweather.herokuapp.com/weather/${inputValue}`);
 
-// let key = prompt("Enter key code: ");
-// let value = prompt("Enter key value: ");
+    try {
 
-// document.cookie = `key:${key},value:${value}`;
-// document.cookie = `key:${encodeURIComponent(key)},value:${encodeURIComponent(value)}`;
+        let response = await p;
+        let response2 = await response.json();
+        data1.innerHTML = `<span>${response2.temperature}</span>`;
+        data1.innerHTML = data1.innerHTML + "<span>🌡️</span>";
 
-// console.log(document.cookie);
-// console.log(decodeURIComponent(key));
-// console.log(decodeURIComponent(value));
+        data2.innerHTML = `<span>${response2.description}</span>`;
+        data2.innerHTML = data2.innerHTML + "<span>⛅</span>";
+
+        data3.innerHTML = `<span>${response2.wind}</span>`;
+        data3.innerHTML = data3.innerHTML + "<span>💨</span>";
+
+    }
+
+    catch (error) {
+        console.log(error)
+    }
+
+    finally {
+        console.log("Done!")
+    }
+}
+
+searchButton.addEventListener("click", getData);
 
 
+cut.addEventListener("click", function () {
+    inputSearch.value = ""
+    cut.style.display = "none"
+    cityName.innerHTML = "'Enter city name'";
+    data1.innerHTML = "";
+    data2.innerHTML = "";
+    data3.innerHTML = "";
+})
 
-let a = ("heyyyyyyyyy;");
-document.cookie = encodeURIComponent(a)
-
-console.log(document.cookie);
-console.log(decodeURIComponent(a))
