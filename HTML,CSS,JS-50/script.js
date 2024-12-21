c1d8146fbb7997c55cffbc723e9a9b6799eb342a
prashant <prashantkumar855114@gmail.com>
2024-12-21 21:52:07 +0530

    IIFE - (immediately invoked async function)

---
+++ HTML,CSS,JS-50/script.js
@@ -0,0 +1,40 @@
+function data(int) {
+    console.log("Getting data...");
+    let promise = new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve(int);
+        }, 2000)
+    })
+
+    return promise;
+}
+
+// async function getData() {
+
+//     let r1 = await data(1);
+//     console.log(r1);
+
+//     let r2 = await data(2);
+//     console.log(r2);
+
+//     let r3 = await data(3);
+//     console.log(r3);
+
+// }
+
+// getData();
+
+
+(async function () {
+
+    let r1 = await data(1);
+    console.log(r1);
+
+    let r2 = await data(2);
+    console.log(r2);
+
+    let r3 = await data(3);
+    console.log(r3);
+
+})();
+
