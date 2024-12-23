9dec06ffdff0e3e0cd32062f777396089ddc2160
prashant <prashantkumar855114@gmail.com>
2024-12-23 13:49:27 +0530

    Hoisting in javaScript

---
+++ HTML,CSS,JS-53/script.js
@@ -0,0 +1,24 @@
+// var a;
+// console.log(a)
+// a=5;
+// console.log(a)
+
+
+/****** HOISTING *******/
+
+console.log(a)
+var a = 5;
+console.log(a)
+
+// console.log(b)
+// let b = 7;                     // throw an error 'cann't access before initialization'
+// const b=7;
+// console.log(b) 
+
+
+greet();
+
+function greet() {
+    console.log('Good morning')
+}
+
