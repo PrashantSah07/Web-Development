98502c9fa25ed81d59dbdb8774c01d7aa74fcd68
prashant <prashantkumar855114@gmail.com>
2024-12-28 23:10:23 +0530

    Regular Expressions

---
+++ HTML,CSS,JS-56/script.js
@@ -0,0 +1,16 @@
+// let text = 'prashant is a very very good and very intelligent boy';
+// let newText=text.replace('very','VERY');          // change only one word.
+// console.log(newText);
+
+// let a = /very/g;
+// let text = 'prashant is a very very good and very intelligent boy';
+// let newText = text.replace(a, 'VERY');          // change all the words.
+// let newText = text.replace(/very/g, 'VERY')
+// console.log(newText);
+
+/* ===== OR ===== */
+
+let text = 'prashant is a very very good and very intelligent boy';
+let newText = text.replaceAll('very', 'VERY');
+console.log(newText);
+
